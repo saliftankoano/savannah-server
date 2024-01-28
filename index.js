7e8b8ca75b6fe1a115c780001ded3bf39de4ac9b
Salif Tankoano <tanksalif@gmail.com>
2024-01-28 13:39:30 -0500

    change access to front-end

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://savannah-uv-server.onrender.com/"
+    "https://savannah-uv.onrender.com/"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
