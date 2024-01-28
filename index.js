20e413671654f104c549f5e43384188de29a694c
Salif Tankoano <tanksalif@gmail.com>
2024-01-28 13:34:36 -0500

    change cors localhost

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://savannah-uv-server.onrender.com/"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
